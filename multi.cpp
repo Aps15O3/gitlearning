648a97ab5ae942ff7cbe2f1b1ed46e2a04713059
Ansh Pratap Singh <anshpratap3@gmail.com>
2021-12-29 02:03:57 +0530

    add multiply

---
+++ multi.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;//imported namespace std if not imported use std::cout or std::cin
+
+
+int main()
+{
+    int a=1;
+    int b=2;
+    int c=a*b;
+    cout << c+1;
+    
+    
+   
+    return 0;
+}
